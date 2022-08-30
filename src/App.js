d7cff839a00dc32ee8bfa1529130916b9b5ecc2b
Lorena Garcia <lorena_65_11@hotmail.com>
2022-08-30 20:24:04 +0200

    3rd Iteration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import "./App.css";
+import allContacts from "./contacts.json";
+import {useState} from "react";
+import { v4 as uuidv4 } from 'uuid';
+
+
+const firstFive = allContacts.slice(0,5)
 
 function App() {
+  const [celebs, setCelebs] = useState(firstFive)
+  console.log(firstFive)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <table>
+      <thead>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won Oscar</th>
+          <th>Won Emmy</th>
+        </tr>
+      </thead>
+      <tbody>
+      {celebs.map((singleCeleb)=>{
+        return(
+          <tr key= {uuidv4()}>
+          <td><img src={singleCeleb.pictureUrl} style={{height: "200px"}}></img></td>
+          <td>{singleCeleb.name}</td>
+          <td>{singleCeleb.popularity}</td>
+          <td>{(singleCeleb.wonOscar ? "🏆" : "")}</td>
+          <td>{(singleCeleb.wonEmmy ? "🏆" : "")}</td>
+        </tr>
+        )
+      })}
+      </tbody>
+    </table>
     </div>
+    
+  /*<div className="App">
+    <h1>Hello</h1>
+    <div className="celebDiv">
+    {celebs.map(celebrity => <Celeb key= {uuidv4()} pictureUrl= {celebrity.pictureUrl} name= {celebrity.name} popularity= {celebrity.popularity} />)}
+    </div>
+  </div>*/
   );
 }
-
 export default App;
